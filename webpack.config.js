a69c5e288c434823d4a406cb878e02d6faa1f321
havefive <lzc09008@gmail.com>
2017-08-22 22:53:04 +0800

    sass remove postcss-loader

--- webpack.config.js
+++ webpack.config.js
@@ -73,7 +73,7 @@ module.exports = {
             use: ExtractTextPlugin.extract({
                 fallback: 'style-loader',
               //resolve-url-loader may be chained before sass-loader if necessary 
-              use: ['css-loader', 'sass-loader','postcss-loader']
+              use: ['css-loader', 'sass-loader']
           })
         }
         ]
